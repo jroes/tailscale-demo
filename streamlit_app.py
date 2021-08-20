70ea7de867dd1a71c38d8d26d8a19b4625db2803
Jon Roes <jon@streamlit.io>
2021-08-20 16:22:47 -0400

    hack

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,14 +10,21 @@ st.title(f"Tailscale demo")
 ephemeral_key = st.text_input("Ephemeral key")
 
 if st.button("Connect"):
-    process = subprocess.run("/app/tailscale-demo/connect.sh", capture_output=True, env={ "TS_KEY": ephemeral_key }, cwd=cwd)
-    st.write(process.stdout)
-    st.write(process.stderr)
-    if process.returncode == 0:
-        st.balloons()
-        st.header("You're connected!")
-        # TODO: Disable the button
-        # TODO: Enable a button to query the db
-    else:
-        st.write("Sorry it didn't work for some reason :(")
+    daemonproc = subprocess.Popen(["/app/tailscale-demo/tailscaled", "--tun=userspace-networking",
+        "--socket=/tmp/tailscale.sock", "--state /tmp/tailscale"])
+
+    clientproc = subprocess.Popen(["/app/tailscale-demo/tailscale", "--socket=/tmp/tailscale.sock", "up",
+        "--authkey=" + ephemeral_key])
+
+    if st.button("Check connection"):
+        st.write("Daemon: " + daemonproc.poll())
+        st.write("Client: " + clientproc.poll())
+
+#    if process.returncode == 0:
+#        st.balloons()
+#        st.header("You're connected!")
+#        # TODO: Disable the button
+#        # TODO: Enable a button to query the db
+#    else:
+#        st.write("Sorry it didn't work for some reason :(")
 
