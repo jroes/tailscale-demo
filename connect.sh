a684064280ac3dc76766e7a100de6da7be75724e
Jon Roes <jon@streamlit.io>
2021-08-20 16:23:02 -0400

    remove connect.sh

--- connect.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Inspired by https://github.com/QuinnyPig/tailscale-layer/blob/main/extension1.sh
-
-./tailscaled --tun=userspace-networking --socket=/tmp/tailscale.sock --state /tmp/tailscale &
-./tailscale --socket=/tmp/tailscale.sock up --authkey=$KEY
