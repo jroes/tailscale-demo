46a9cae2abfcc7b9a2cd63c90b7d83431f9cd29c
Jon Roes <jon@streamlit.io>
2021-08-20 16:13:18 -0400

    hack

--- connect.sh
+++ connect.sh
@@ -3,7 +3,4 @@
 # Inspired by https://github.com/QuinnyPig/tailscale-layer/blob/main/extension1.sh
 
 ./tailscaled --tun=userspace-networking --socket=/tmp/tailscale.sock --state /tmp/tailscale &
-until ./tailscale --socket=/tmp/tailscale.sock up --authkey=$KEY
-do
-    sleep 0.1
-done
+./tailscale --socket=/tmp/tailscale.sock up --authkey=$KEY
