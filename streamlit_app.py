d8e8f85e61627ddb9b7a573660f9e763fa2f04d6
Jon Roes <jon@streamlit.io>
2021-08-20 17:52:50 -0400

    hack

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,13 +10,12 @@ st.title(f"Tailscale demo")
 ephemeral_key = st.text_input("Ephemeral key")
 
 if st.button("Initialize Tailscale"):
-    st.write(ephemeral_key)
     subprocess.Popen(["/app/tailscale-demo/tailscaled", "--tun=userspace-networking",
         "--socket=/tmp/tailscale.sock", "--state=/tmp/tailscale",
         "--socks5-server=localhost:1055"])
-    subprocess.Popen(["/app/tailscale-demo/tailscale", "--authkey=" + ephemeral_key,
+    subprocess.Popen(["/app/tailscale-demo/tailscale",
         "--socket=/tmp/tailscale.sock",
-        "up",
+        "up", "--authkey=" + ephemeral_key,
         "--hostname=tailscale-demo"])
 
 if st.button("Check connection"):
