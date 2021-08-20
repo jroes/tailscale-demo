27695cc9f7226a230fd4310a974fb327b7bf6a6b
Jon Roes <jon@streamlit.io>
2021-08-20 16:24:32 -0400

    hack

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,8 @@ st.title(f"Tailscale demo")
 
 ephemeral_key = st.text_input("Ephemeral key")
 
+daemonproc = None
+clientproc = None
 if st.button("Connect"):
     daemonproc = subprocess.Popen(["/app/tailscale-demo/tailscaled", "--tun=userspace-networking",
         "--socket=/tmp/tailscale.sock", "--state /tmp/tailscale"])
@@ -16,9 +18,9 @@ if st.button("Connect"):
     clientproc = subprocess.Popen(["/app/tailscale-demo/tailscale", "--socket=/tmp/tailscale.sock", "up",
         "--authkey=" + ephemeral_key])
 
-    if st.button("Check connection"):
-        st.write("Daemon: " + daemonproc.poll())
-        st.write("Client: " + clientproc.poll())
+if st.button("Check connection"):
+    st.write("Daemon: " + daemonproc.poll())
+    st.write("Client: " + clientproc.poll())
 
 #    if process.returncode == 0:
 #        st.balloons()
