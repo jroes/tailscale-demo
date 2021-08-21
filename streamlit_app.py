f625729a9956d744d574132348e08be6125ff145
Jon Roes <jon@streamlit.io>
2021-08-20 22:18:01 -0400

    hack

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,11 +8,11 @@ st.title(f"Tailscale demo")
 if st.button("Initialize Tailscale"):
     subprocess.Popen(["/app/tailscale-demo/tailscaled", "--tun=userspace-networking",
         "--socket=/tmp/tailscale.sock", "--state=/tmp/tailscale",
-        "--socks5-server=localhost:1055"])
+        "--socks5-server=localhost:1055"], env={"ALL_PROXY": ""})
     subprocess.Popen(["/app/tailscale-demo/tailscale",
         "--socket=/tmp/tailscale.sock",
         "up", "--authkey=" + os.getenv('TAILSCALE_AUTHKEY'),
-        "--hostname=tailscale-demo"])
+        "--hostname=tailscale-demo"], env={"ALL_PROXY": ""})
 
 if st.button("Check connection"):
     os.system("/app/tailscale-demo/tailscale --socket=/tmp/tailscale.sock status")
