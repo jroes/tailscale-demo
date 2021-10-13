0a69f22331c9508ff265b5f814c412d6049939c2
Jon Roes <jon@streamlit.io>
2021-10-13 18:24:47 -0400

    ssh tunnelling

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,14 +4,27 @@ import subprocess
 import psycopg2
 import pandas.io.sql as psql
 
-def init_tunnel():
-    os.system(f"mkdir -p ~/.ssh && chmod 700 ~/.ssh")
-    with open("/home/appuser/.ssh/key", "w") as f:
-        f.write(st.secrets["SSH_AUTHKEY"])
-    os.chmod("/home/appuser/.ssh/key", 0o600) # user read/write only
-    os.system(f"mkdir -p ~/.ssh && ssh-keyscan -H {st.secrets['SSH_HOST']} >> ~/.ssh/known_hosts")
-    subprocess.Popen(["ssh", "-i", "~/.ssh/key", "-4", "-N", "-L", f"54321:localhost:5432", f"{st.secrets['SSH_USER']}@{st.secrets['SSH_HOST']}"])
-    #os.remove("~/.ssh/key") # no need to keep on disk
+
+class SSHTunnel():
+    def initialize(self):
+        self.proc = None
+
+    def connect(self):
+        os.system(f"mkdir -p ~/.ssh && chmod 700 ~/.ssh")
+        with open("/home/appuser/.ssh/key", "w") as f:
+            f.write(st.secrets["SSH_AUTHKEY"])
+        os.chmod("/home/appuser/.ssh/key", 0o600) # user read/write only
+        os.system(f"mkdir -p ~/.ssh && ssh-keyscan -H {st.secrets['SSH_HOST']} >> ~/.ssh/known_hosts")
+        self.proc = subprocess.Popen(["ssh", "-i", "~/.ssh/key", "-4", "-N", "-L", f"54321:localhost:5432", f"{st.secrets['SSH_USER']}@{st.secrets['SSH_HOST']}"])
+        #os.remove("~/.ssh/key") # no need to keep on disk
+
+    def is_connected(self):
+        if self.proc is not None:
+            return True
+        else:
+            return False
+
+tunnel = SSHTunnel()
 
 @st.experimental_singleton
 def connect():
@@ -20,9 +33,11 @@ def connect():
 st.title(f"Tailscale & SSH demo")
 
 st.header("SSH tunnel")
-if st.button("Initialize SSH tunnel"):
-    init_tunnel()
+if st.button("Connect to SSH tunnel"):
+    tunnel.connect()
 
+if tunnel.is_connected():
+    st.write(proc)
 
 ## Tailscale stuff
 
